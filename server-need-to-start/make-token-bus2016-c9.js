9b51d4fd11b18a4ac0fc28fa6e8a2b4022491f7b
twoutlook <twcloudwebapp@outlook.com>
2016-01-17 23:42:43 +0000

    using c9,https://ide.c9.io/twoutlook/webapp-nodejs2016,change fb to bus2016-c9

---
+++ server-need-to-start/make-token-bus2016-c9.js
@@ -0,0 +1,18 @@
+/*
+eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ2IjowLCJkIjp7InVpZCI6Im1hcmsiLCJzb21lIjoiYXJiaXRyYXJ5IiwiZGF0YSI6ImhlcmUifSwiaWF
+0IjoxNDUyOTQ5OTQ1fQ.68ett9WCwQzrY019k2vhLy_peiuGs-wf_Pantsu9htw
+*/
+//  ".write": "auth.uid == 'mark'"
+// https://bus2016.firebaseio.com
+//var SECRET="rwAvX4APSO07SrtMiSpDblrxx45cZmHRXPhsevhb";
+
+//https://bus2016-c9.firebaseio.com
+var SECRET="tr4zEwKz6LdwoPyvNdkttb4KMCkHASKTjxWHSQ0x";
+      
+var uid="mark";
+
+// No need to change following
+var FirebaseTokenGenerator = require("firebase-token-generator");
+var tokenGenerator = new FirebaseTokenGenerator(SECRET);
+var token = tokenGenerator.createToken({ uid:uid, some: "arbitrary", data: "here" });
+console.log("\n\n"+token);
