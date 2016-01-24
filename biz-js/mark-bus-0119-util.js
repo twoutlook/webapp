997e98eca1eb08963ded06a3b70688a5030117b5
Mark <twcloudwebapp@outlook.com>
2016-01-24 11:34:56 +0800

    fine tune,marker's font

--- biz-js/mark-bus-0119-util.js
+++ biz-js/mark-bus-0119-util.js
@@ -85,7 +85,7 @@ function makeMarkerV2(bus,unix,lat,lon,icon,toOpenNow,toMoveCenterNow,cnt,msg,ro
   var contentString = '<div id="content" style="background-color:#f9f9f9"> '+
         '<div id="siteNotice">'+
         '</div>'+
-        '<h1 id="firstHeading" class="firstHeading">'+" <u> "+routeName+" </u> "+'</h1>'+
+        '<h4 id="firstHeading" class="firstHeading">'+" <u> "+routeName+" </u> "+'</h4>'+
         '<div id="bodyContent" class="firstHeading">'+
         // '<p><b>'+$scope.unix+'</b>'+
         // '<p><b>'+"【"+bus+"】"+cntStr+"<BR>"+dt2+'</b>'+msg+
