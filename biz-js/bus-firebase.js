d7bdbbddc4e8d4af1141d19498afbabf51a1953d
Mark <twcloudwebapp@outlook.com>
2016-02-10 00:36:12 +0800

    remark bus-firebase.js

--- biz-js/bus-firebase.js
+++ biz-js/bus-firebase.js
@@ -3,3 +3,7 @@ var urlFirebase = "https://bus-0119.firebaseio.com/";
 var ref = new Firebase(urlFirebase);
 var refRoutes = ref.child("routesv5/");
 var refBuslist = ref.child("buslistv6/");
+
+//https://bus-0119.firebaseio.com/buslistv8-doc3  --- for 3 dots
+
+//buslistv9-doc' + x    --- for 30 dots
