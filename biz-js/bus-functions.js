ed6e0e27859acd853b25ab134f8a643ebd6105e8
Mark <twcloudwebapp@outlook.com>
2016-02-09 22:44:52 +0800

    bus info window, bus serial number to update dt

--- biz-js/bus-functions.js
+++ biz-js/bus-functions.js
@@ -6,9 +6,11 @@ function openMapUrl(url){
 
 
 //中間按鈕
-function resetRouteBuses() {
-  console.log("DOING...resetRouteBuses");
-    //
+function resetRouteBuses(num) {
+  console.log("DOING...resetRouteBuses===>"+num);
+  selectRouteAndBus(num);
+
+  //DOING
   // watch_bus_array=[];
   return;
   watch_bus_index++;
@@ -88,7 +90,7 @@ mem_majorRouteId=majorRouteId;
                         + "<div>"
                         + '<h4><button style="; border-radius: 6px; background-color:#D9544F; color:white" type="button" onclick="show30Dots(\'' + bus.bus + '\')">' + bus.bus + '</button></h4>'
                         + dt2
-                        + '<h4><button style="; border-radius: 6px; background-color:#5BB85D; color:white" type="button" onclick="resetRouteBuses()">' +"第" + (busCnt)+"輛" + '</button></h4>'
+                        + '<h4><button style="; border-radius: 6px; background-color:#5BB85D; color:white" type="button" onclick="resetRouteBuses(\'' + busCnt + '\')">' +"第" + (busCnt)+"輛" + '</button></h4>'
                         + '<h4><button style="; border-radius: 6px; background-color:#327DB4; color:white" type="button" onclick="openMapUrl(\'' + route_url[mySelect.value] + '\')">' + route_name[mySelect.value] + '</button></h4>'
                         + "</div>"
                         ;
@@ -111,7 +113,87 @@ mem_majorRouteId=majorRouteId;
     }); // end of once
 }
 
+// NOTE:重畫路線和車輛
+function selectRouteAndBus(num) {
+    anchorCnt=99;
+    initMap();
+    watch_bus_array=[];
+    watch_bus_index=-1;
 
+    // alert(mySelect.value+" "+route_name[mySelect.value]);
+    console.log("selectRouteAndBus()");
+    $("#list").hide();
+
+    var busCnt = 0;
+    document.getElementById("busCnt").innerHTML = busCnt + "輛公車";
+    var route = parseInt(mySelect.value);// routeId 過來時是文本,要轉成 integer
+
+    refBuslist.once("child_added", function (snapshot, prevChildKey) { // 就只有一個doc
+        var key = snapshot.key();
+        var val = snapshot.val();
+        var array = val;
+        var toOpen = true;
+        var showRouteAlready = false;
+        for (var i = 0; i < array.length; i++) {
+            var bus = array[i];
+            if (bus['route'] == route) {
+
+              save_bus[busCnt]=bus;
+
+                busCnt++; //指定切換的車輛數
+                document.getElementById("busCnt").innerHTML = busCnt + "輛公車";
+
+                watch_bus_array[busCnt]=bus['bus'] ;
+                watch_bus_index=1;
+
+                // NOTE:MAP URL 的ID值,剛好就是 Major Route
+                var temp1 = route_url[mySelect.value].split("=");
+                var majorRouteId = parseInt(temp1[1]);
+
+mem_majorRouteId=majorRouteId;
+
+
+                // NOTE:路線上的站牌只要畫一次
+                if (!showRouteAlready) {
+                    getRouteDots(majorRouteId);
+                    showRouteAlready = true;
+                }
+
+
+                var dt = new Date(parseInt(bus.unix));
+                var dt2 = dt.format("yyyy-mm-dd<br><b>HH:MM</b>:ss");
+                var msg = ""
+                        // + "<div><h4>"
+                        + "<div>"
+                        + '<h4><button style="; border-radius: 6px; background-color:#D9544F; color:white" type="button" onclick="show30Dots(\'' + bus.bus + '\')">' + bus.bus + '</button></h4>'
+                        + dt2
+                        + '<h4><button style="; border-radius: 6px; background-color:#5BB85D; color:white" type="button" onclick="resetRouteBuses(\'' +(busCnt)+ '\')">' +"第" + (busCnt)+"輛" + '</button></h4>'
+                        + '<h4><button style="; border-radius: 6px; background-color:#327DB4; color:white" type="button" onclick="openMapUrl(\'' + route_url[mySelect.value] + '\')">' + route_name[mySelect.value] + '</button></h4>'
+                        + "</div>"
+                        ;
+                // makeMarkerV2(bus.bus, bus.unix, bus.lat, bus.lon, iconBus, toOpen, true, null, msg);
+                toOpen = false;
+
+                if (busCnt==num) {
+
+                 toOpen = true;
+                }
+                makeMarkerV3___toFro(bus.bus, bus.unix, bus.lat, bus.lon, iconBus, toOpen, true, null, msg);
+            }
+        }
+
+
+        // console.log(watch_bus_array);
+        // console.log(watch_bus_index);
+
+        // NOTE:當路線沒有車時,顯示地標
+        if (busCnt == 0) {
+            showAnchor();
+        } else {
+            // map.setCenter({lat: $scope.lat, lng: $scope.lon});
+        }
+    }); // end of once
+}
 
 function selectRouteByAddOne() {
 
@@ -156,6 +238,12 @@ function selectRouteByAddOne() {
                         + '<h4><button style="; border-radius: 6px; background-color:#327DB4; color:white" type="button" onclick="openMapUrl(\'' + route_url[mySelect.value] + '\')">' + route_name[mySelect.value] + '</button></h4>'
                         + "</div>"
                         ;
+
+
+
+
+
+
                 makeMarkerV2(bus.bus, bus.unix, bus.lat, bus.lon, iconBus, toOpen, true, null, msg);
                 // toOpen = false;
                 document.getElementById("busCnt").innerHTML = watch_bus_array.length-1 + "輛第"+watch_bus_index+"輛";
